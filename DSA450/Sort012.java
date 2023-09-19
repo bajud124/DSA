84754dae447bb2ec87fdc7fac4dcc5006daea1cc
Vatsal Golakiya <vgolakiya@csod.com>
2023-09-19 23:04:47 +0530

    dsa 450

---
+++ DSA450/Sort012.java
@@ -0,0 +1,35 @@
+package DSA450;
+
+public class Sort012 {
+    public static void sort012(int a[], int n)
+    {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int value : a) {
+            if (value == 0) {
+                count0++;
+            } 
+            else if (value == 1) {
+                count1++;
+            }
+            else if (value == 2) {
+                count2++;
+            }
+            
+        }
+        int index = 0;
+        while(count0 > 0) {
+            a[index++] = 0;
+            count0--;
+        }
+        while(count1 > 0) {
+            a[index++] = 1;
+            count1--;
+        }
+        while(count2 > 0) {
+            a[index++] = 2;
+            count2--;
+        }
+    }
+}
